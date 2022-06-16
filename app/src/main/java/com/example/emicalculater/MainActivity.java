30f693dc2b7a32d88cb9b8e3a3216a15941a1806
desai <desaigunavant3219@gmail.com>
2022-06-16 08:21:59 +0530

    15-6-22

--- app/src/main/java/com/example/emicalculater/MainActivity.java
+++ app/src/main/java/com/example/emicalculater/MainActivity.java
@@ -7,14 +7,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    String data = "";
+    String data = "", dataA, dataI, dataY, dataM;
+    int Year = 0;
+    double Loan_Amount, Intrest, AM, IntrestAns, Total, EMI, Divide, IM, IM1, Yearly_EMI, Total_Intrest, Total_payment;
     Button btncalculate, btnreset, btndetail;
-    TextView text1, text2, text3, text4, text5, text6, text7, text8, text9, text10, text11, text12;
-    EditText edttext1, edttext2, edttext3, edttext4, edttext5;
+    TextView text1, text2, text3, text4, text5, text6, text7, text8, text9, text10, text11, text12, edttext5;
+    EditText edttext1, edttext2, edttext3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initBinding();
         initOnClick();
-        calculate();
 
     }
 
@@ -37,7 +39,6 @@ public class MainActivity extends AppCompatActivity {
         edttext1 = findViewById(R.id.edttext1);
         edttext2 = findViewById(R.id.edttext2);
         edttext3 = findViewById(R.id.edttext3);
-        edttext4 = findViewById(R.id.edttext4);
         edttext5 = findViewById(R.id.edttext5);
         text5 = findViewById(R.id.text5);
         text6 = findViewById(R.id.text6);
@@ -50,33 +51,67 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    void initOnClick()
-
-    {
+    void initOnClick() {
         btnreset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 edttext1.setText("");
                 edttext2.setText("");
                 edttext3.setText("");
-                edttext4.setText("");
+                text9.setText("");
+                text10.setText("");
+                text11.setText("");
+                text12.setText("");
             }
         });
-
-    }
-    void calculate()
-    {
-        int a;
         btncalculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                dataA = edttext1.getText().toString();
+                dataI = edttext2.getText().toString();
+                dataY = edttext3.getText().toString();
+                dataM = edttext3.getText().toString();
+                Loan_Amount = Double.parseDouble(dataA);
+                Intrest = Double.parseDouble(dataI);
+                Year = Integer.parseInt(dataY);
+
+                AM = Year * 12;
+                IntrestAns = Intrest / 12 / 100;
+                IM = Math.pow(1 + IntrestAns, AM);
+                IM1 = Math.pow(1 + IntrestAns, AM) - 1;
+                Divide = IM / IM1;
+                Total = Loan_Amount * IntrestAns * Divide;
+                EMI = Total;
+
+                Yearly_EMI = EMI * 12;
+                Total_Intrest = (Loan_Amount * Intrest * Year) / 100;
+                Total_payment = Total_Intrest + Loan_Amount;
 
 
+                if {
+                    Toast.makeText(MainActivity.this, ("Enter year"), Toast.LENGTH_LONG).show();
 
+                }
+
+                edttext5.setText("" + EMI);
             }
-        });
+            });
+
+
+                btndetail.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        text9.setText("" + EMI);
+                        text10.setText("" + Yearly_EMI);
+                        text11.setText("" + Total_Intrest);
+                        text12.setText("" + Total_payment);
+                    }
+                });
+
+
+
+
+
     }
-
-
 }
